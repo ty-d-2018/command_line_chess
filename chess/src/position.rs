3cbd547eef431b4703d54a5791f730628c35c2c5
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-11-24 15:18:01 -0700

    working on movement. And determining slant.

--- chess/src/position.rs
+++ chess/src/position.rs
@@ -10,4 +10,20 @@ pub trait Location{
     fn get_residents(&self) -> HashMap::<bool, Vec::<impl Location>>;
     fn can_move(&self) -> bool;
     fn flip(&mut self, location: &impl Location) -> Result::<(), BoardResponse>;
+    fn get_name(&self) -> String;
+}
+
+pub struct Movement;
+
+impl Movement{
+    fn find_slant(label: &String) -> Option::<(bool, bool)>{
+        match label.as_str(){
+            "up-right" => Some((true, true)),
+            "up-left" => Some((false, true)),
+            "down-left" => Some((false, false)),
+            "down-right" => Some((true, false)),
+            _ => None
+        }
+    }
+    pub fn slant(piece: &impl Location){}
 }
