0f20c860082fe2705cb4b594c860fdc3378caf6f
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-11-24 14:03:06 -0700

    simplified can_move return value

--- chess/src/position.rs
+++ chess/src/position.rs
@@ -8,6 +8,6 @@ pub enum BoardResponse{
 pub trait Location{
     fn get_grid_number(&self) -> usize;
     fn get_residents(&self) -> HashMap::<bool, Vec::<impl Location>>;
-    fn can_move(&self) -> Option::<bool>;
+    fn can_move(&self) -> bool;
     fn flip(&mut self, location: &impl Location) -> Result::<(), BoardResponse>;
 }
