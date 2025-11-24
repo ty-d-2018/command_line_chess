fcc78aad00cf8dd491b00df5e33893a24f99dbd6
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-11-24 14:01:44 -0700

    added flip to move positions

--- chess/src/position.rs
+++ chess/src/position.rs
@@ -1,7 +1,13 @@
 use std::collections::HashMap;
 
+pub enum BoardResponse{
+    CannotMoveToPosition,
+    ThisCellIsEmpty,
+}
+
 pub trait Location{
     fn get_grid_number(&self) -> usize;
     fn get_residents(&self) -> HashMap::<bool, Vec::<impl Location>>;
     fn can_move(&self) -> Option::<bool>;
+    fn flip(&mut self, location: &impl Location) -> Result::<(), BoardResponse>;
 }
